2eb3c6725a8b183cd7d3b408b32c2786c24e868a
subbuthripurari-cyber <subbuthripurari@gmail.com>
2025-10-27 14:04:58 +0530

    Add files via upload

---
+++ ARITHMETIC OPERATIONS.cpp
@@ -0,0 +1,16 @@
+// ARITHMETIC OPERATOR
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    int a = 20, b = 10;
+
+    printf("%d\n", a + b); // Addition
+    printf("%d\n", a - b); // Subtraction
+    printf("%d\n", a * b); // Multiplication
+    printf("%d\n", a / b); // Division
+    printf("%d\n", a % b); // Modulus
+
+    return 0;
+}
+
